678b1f90274242af6b509ec3cbbc596d57bf0b54
Lucas Baquey <l.baquey@syloria.fr>
2026-08-21 23:10:15 +0200

    PS-007-FIX: prohibit copy operations of AfeSession

--- include/afe_session.hpp
+++ include/afe_session.hpp
@@ -7,6 +7,9 @@ namespace bms
     public:
         AfeSession(); // Constructor
         ~AfeSession(); // Destructor
+
+        AfeSession(const AfeSession&) = delete; // Forbid copy construction AfeSession session2(session1);
+        AfeSession& operator=(const AfeSession&) = delete; // Forbid copy assignment session2 = session1;
     };
 
 } // namespace bms
