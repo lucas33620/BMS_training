c31184b8d203ded7f8f0e6deca845f93a5e0a83d
Lucas Baquey <l.baquey@syloria.fr>
2026-08-29 15:24:23 +0200

    PS-009: Introduce multi-cell battery supervision

--- include/battery_pack.hpp
+++ include/battery_pack.hpp
@@ -5,11 +5,6 @@
 
 namespace bms
 {
-    enum class BatteryPackResult : uint8_t
-    {
-        OK = 0,
-        INVALID_INDEX
-    };
 
     class BatteryPack
     {
@@ -18,7 +13,7 @@ namespace bms
         static constexpr std::size_t CELL_COUNT = 4;
 
         const std::array<CellVoltageData, CELL_COUNT>& get_cells() const;
-        BatteryPackResult set_cell(std::size_t index, const CellVoltageData& data);
+        bool set_cell(std::size_t index, const CellVoltageData& data);
 
     private :
         std::array<CellVoltageData, CELL_COUNT> cells; // cells have exactly 4 éléments of type CellVoltageData (std::array<T, N>)
--- src/battery_monitor.cpp
+++ src/battery_monitor.cpp
@@ -1,32 +1,95 @@
 #include "battery_monitor.hpp"
 
-bms::BatteryMonitor::BatteryMonitor(ICellMonitor& reader)
-    :reader(reader)
+namespace bms
 {
-}
-
-bms::CellVoltageStatus bms::BatteryMonitor::check_cell_voltage() const
-{
-    CellVoltageData data = reader.get_measurement();
-
-    return check_cell_voltage(data);    
-}
-
-bms::CellVoltageStatus bms::BatteryMonitor::check_cell_voltage(const CellVoltageData& data) const
-{
-    if (data.validity == CellMeasurementValidity::INVALID)
+    BatteryMonitor::BatteryMonitor(ICellMonitor& reader)
+        :reader(reader)
     {
-        return CellVoltageStatus::INVALID;
     }
 
-    if (data.voltage < 3000)
+    CellVoltageStatus BatteryMonitor::check_cell_voltage() const
     {
-        return bms::CellVoltageStatus::UNDERVOLTAGE;
+        CellVoltageData data = reader.get_measurement();
+
+        return check_cell_voltage(data);    
     }
-    else if (data.voltage > 4200)
+
+    CellVoltageStatus BatteryMonitor::check_cell_voltage(const CellVoltageData& data) const
     {
-        return bms::CellVoltageStatus::OVERVOLTAGE;
+        if (data.validity == CellMeasurementValidity::INVALID)
+        {
+            return CellVoltageStatus::INVALID;
+        }
+
+        if (data.voltage < 3000)
+        {
+            return CellVoltageStatus::UNDERVOLTAGE;
+        }
+        else if (data.voltage > 4200)
+        {
+            return CellVoltageStatus::OVERVOLTAGE;
+        }
+        return CellVoltageStatus::NORMAL;
+        
     }
-    return bms::CellVoltageStatus::NORMAL;
-    
+
+    BatteryPackResult BatteryMonitor::check_battery_pack(const BatteryPack& pack) const
+    {
+        uint16_t voltage_min = 0;
+        uint16_t voltage_max = 0;
+
+        CellVoltageStatus pack_status = CellVoltageStatus::NORMAL;
+        bool has_valid_measurement = false;
+
+        const auto& cells = pack.get_cells();
+
+        for (std::size_t i = 0; i < BatteryPack::CELL_COUNT; ++i)
+        {
+            const CellVoltageStatus cell_status = check_cell_voltage(cells[i]);
+
+            if (cell_status == CellVoltageStatus::INVALID)
+            {
+                pack_status = CellVoltageStatus::INVALID;
+                continue;
+            }
+
+            if (cell_status == CellVoltageStatus::OVERVOLTAGE &&
+                pack_status != CellVoltageStatus::INVALID)
+            {
+                pack_status = CellVoltageStatus::OVERVOLTAGE;
+            }
+            else if (cell_status == CellVoltageStatus::UNDERVOLTAGE &&
+                    pack_status != CellVoltageStatus::INVALID &&
+                    pack_status != CellVoltageStatus::OVERVOLTAGE)
+            {
+                pack_status = CellVoltageStatus::UNDERVOLTAGE;
+            }
+
+            if (!has_valid_measurement)
+            {
+                voltage_min = cells[i].voltage;
+                voltage_max = cells[i].voltage;
+                has_valid_measurement = true;
+            }
+            else
+            {
+                if (cells[i].voltage < voltage_min)
+                {
+                    voltage_min = cells[i].voltage;
+                }
+
+                if (cells[i].voltage > voltage_max)
+                {
+                    voltage_max = cells[i].voltage;
+                }
+            }
+        }
+
+        return BatteryPackResult{
+            pack_status,
+            voltage_min,
+            voltage_max
+        };
+    }
+
 }
