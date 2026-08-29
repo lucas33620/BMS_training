bbfcef136fefe6ae0b35d3d295d35ff4687b8cb0
Lucas Baquey <l.baquey@syloria.fr>
2026-08-29 16:13:12 +0200

    PS-010: add generic min and max template

--- include/numeric_utils.hpp
+++ include/numeric_utils.hpp
@@ -3,7 +3,6 @@
 namespace bms
 {
     template <typename T>
-
     T clamp_value(T value, T min_value, T max_value)
     {
         if (value < min_value)
@@ -18,5 +17,41 @@ namespace bms
         {
             return value;
         }
+    }
+
+    template <typename T, std::size_t N>
+    T min_value(const std::array<T, N>& arr)
+    {
+        static_assert(N > 0, "min_value requires a non-empty array");
+
+        T min_val = arr[0];
+
+        for (std::size_t i = 1; i < N; ++i)
+        {
+            if (arr[i] < min_val)
+            {
+                min_val = arr[i];
+            }
+        }
+
+        return min_val;
+    }
+
+    template <typename T, std::size_t N>
+    T max_value(const std::array<T, N>& arr)
+    {
+        static_assert(N > 0, "max_value requires a non-empty array");
+
+        T max_val = arr[0];
+
+        for (std::size_t i = 1; i < N; ++i)
+        {
+            if (arr[i] > max_val)
+            {
+                max_val = arr[i];
+            }
+        }
+
+        return max_val;
     }
 }
